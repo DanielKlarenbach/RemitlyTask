2cbc5a6968fff8e91bbfb8f2db23791843d5a7b4
Daniel Klarenbach <daniel.klarenbach@gmail.com>
2021-03-06 14:57:18 +0100

    test: Edited RemityTaskApplicationTests.java - edited contextLoads() method so that it check if controller was created

--- src/test/java/com/herokuapp/remitytask/remitytask/RemityTaskApplicationTests.java
+++ src/test/java/com/herokuapp/remitytask/remitytask/RemityTaskApplicationTests.java
@@ -1,13 +1,21 @@
 package com.herokuapp.remitytask.remitytask;
 
+import com.herokuapp.remitytask.remitytask.Controllers.GBPToPLNController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class RemityTaskApplicationTests {
 
+    @Autowired
+    private GBPToPLNController controller;
+
     @Test
-    void contextLoads() {
+    void contextLoads() throws Exception {
+        assertThat(controller).isNotNull();
     }
 
 }
