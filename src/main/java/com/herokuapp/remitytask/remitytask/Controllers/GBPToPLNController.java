820a8cf9cac9e69cef65c282e3a55aa4198f443b
Daniel Klarenbach <daniel.klarenbach@gmail.com>
2021-03-06 16:20:17 +0100

    Added test to check if GBPToPLN api endpoint returns Float value

--- src/main/java/com/herokuapp/remitytask/remitytask/Controllers/GBPToPLNController.java
+++ src/main/java/com/herokuapp/remitytask/remitytask/Controllers/GBPToPLNController.java
@@ -14,6 +14,4 @@ public class GBPToPLNController {
         return null;
     }
 
-
-    //private Float getGBPToPLNRate()
 }
--- src/test/java/com/herokuapp/remitytask/remitytask/RemityTaskApplicationTests.java
+++ src/test/java/com/herokuapp/remitytask/remitytask/RemityTaskApplicationTests.java
@@ -4,14 +4,22 @@ import com.herokuapp.remitytask.remitytask.Controllers.GBPToPLNController;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class RemityTaskApplicationTests {
+
+    @LocalServerPort
+    private int port;
 
     @Autowired
     private GBPToPLNController controller;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
 
     @Test
     public void contextLoads() throws Exception {
@@ -19,8 +27,9 @@ class RemityTaskApplicationTests {
     }
 
     @Test
-    public void testGetGBPToPLNRate() throws Exception{
-        assertThat(this.controller.)
+    public void testConvertGBPToPLN() throws Exception{
+        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/GBPToPLN/4",
+                Float.class)).isInstanceOf(Float.TYPE);
     }
 
 }
