629092d6865db575224fd69da1271591a792a3ad
Daniel Klarenbach <daniel.klarenbach@gmail.com>
2021-03-07 14:05:43 +0100

    Implementend logic behind frontend in app.component.ts - implemented method convertGBPToPLN which sends GBP value to spring backend and waits for response

--- remitly-task/src/app/app.component.ts
+++ remitly-task/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'remitly-task';
+  GBP : number = 0;
+  PLN : number = 0;
+
+  constructor(private http: HttpClient) { }
+
+  convertGBPToPLN(){
+    let obs = this.http.get<number>('http://localhost:8080/GBPToPLN/'+this.GBP.toString());
+    obs.subscribe((response) => {
+      this.PLN = response;
+    });
+  }
 }
